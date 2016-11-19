1e49bfddc42a9464564205ddb323470ec1c5d910
Jeffrey Dwyer <Jeffrey Dwyer>
2016-11-19 09:57:20 -0800

    Added level 2 intro cutscene

---
+++ General Scripts/Game Mechanics/Stage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Stage
+{
+
+    public Func<bool> progressCheck;
+    public Func<string> setup;
+
+    // Creates a new stage for the level
+    public Stage(Func<string> setup, Func<bool> progressCondition)
+    {
+        this.progressCheck = progressCondition;
+        this.setup = setup;
+    }
+}
---
+++ General Scripts/Grahpical Effects/Float.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Float : MonoBehaviour {
+
+    public float period = 1;
+    public float amp = 1;
+    public float offset = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + new Vector3(0, (amp * 0.0001f) * Mathf.Sin(period * Time.time + offset), 0);
+	}
+
+}
